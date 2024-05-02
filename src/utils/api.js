1a8fa2227f703100aadc30c405c0db20d8ae0ce8
fares <fares.haliim@gmail.com>
2024-05-02 23:37:27 +0300

    add changes ✅

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,13 +2,20 @@ import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
-
+const TMDB_API_KEY = import.meta.env.VITE_REACT_APP_TMDB_API_KEY;
+console.log(TMDB_TOKEN);
+console.log(TMDB_API_KEY);
 export const URL = "https://image.tmdb.org/t/p/original/";
+const headers = {
+  Authorization: `bearer ${TMDB_TOKEN}`,
+};
+
 export async function fetchData(url, params) {
   try {
     const { data } = await axios.get(
-      `${BASE_URL}${url}?api_key=${TMDB_TOKEN}`,
+      `${BASE_URL}${url}?api_key=${TMDB_API_KEY}`,
       {
+        headers,
         params,
       }
     );
